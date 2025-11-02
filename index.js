9d1571600b583a093b6181c6b697ddf25131216a
DanyloSuslinec2010 <danickasuslinec@gmail.com>
2025-11-02 14:19:59 +0100

    ertger

---
+++ index.js
@@ -0,0 +1,105 @@
+const categories = document.querySelectorAll("#categories .item");
+console.log(`У списку ${categories.length} категорії.`);
+
+categories.forEach((category) => {
+  const title = category.querySelector("h2").textContent;
+  const itemsCount = category.querySelectorAll("ul li").length;
+  console.log(`Категорія: ${title}`);
+  console.log(`Кількість елементів: ${itemsCount}`);
+});
+
+
+
+
+
+
+
+
+const ingredients = [
+  "Картопля",
+  "Гриби",
+  "Часник",
+  "Помідори",
+  "Зелень",
+  "Приправи",
+];
+
+const list = document.querySelector("#ingredients");
+
+const items = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  return li;
+});
+
+list.append(...items);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const gallery = document.getElementById("gallery");
+
+const markup = images
+  .map((image) => `<li><img src="${image.url}" alt="${image.alt}"></li>`)
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+
+
+
+
+
+
+
+
+
+
+
+let counterValue = 0;
+
+const valueEl = document.getElementById("value");
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+function updateDisplay() {
+  valueEl.textContent = counterValue;
+}
+
+function increment() {
+  counterValue += 1;
+  updateDisplay();
+}
+
+function decrement() {
+  counterValue -= 1;
+  updateDisplay();
+}
+
+incrementBtn.addEventListener("click", increment);
+decrementBtn.addEventListener("click", decrement);
